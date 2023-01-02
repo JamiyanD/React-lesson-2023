b220362ba19f5caaeca74c600068f29166638dbb
Morning <morning@Lab-02-02s-iMac.local>
2023-01-02 12:40:06 +0800

    01.02 at class

--- day-33/my-react-app-module/src/App.js
+++ day-33/my-react-app-module/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+import logo1 from './img/image-yellow.png';
+import logo2 from './img/image-aqua.png';
+import logo3 from './img/image-steel.png';
+import logo4 from './img/image-rose.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          E\dit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,8 +19,59 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1>Popular products</h1>
       </header>
+      <hr></hr>
+      <div id="container">
+        <img id="image" src={logo1}></img>
+        <div id="text-">
+          <i className="fa-solid fa-triangle"></i>
+          <h1>55</h1>
+          <p>Haught or Naught</p>
+          <p>High-minded or absent-minded?You decide.</p>
+          <div id='profile'>
+            <p>Submitted by: </p>
+            <img id="profile-img" src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"></img>
+          </div>
+        </div>
+      </div>
+      <div id="container">
+        <img id="image" src={logo2}></img>
+        <div id="text-container">
+          <h1>55</h1>
+          <p>Haught or Naught</p>
+          <p>High-minded or absent-minded?You decide.</p>
+          <div id='profile'>
+            <p>Submitted by: </p>
+            <img id="profile-img" src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"></img>
+          </div>
+        </div>
+      </div>
+      <div id="container">
+        <img id="image" src={logo3}></img>
+        <div id="text-container">
+          <h1>55</h1>
+          <p>Haught or Naught</p>
+          <p>High-minded or absent-minded?You decide.</p>
+          <div id='profile'>
+            <p>Submitted by: </p>
+            <img id="profile-img" src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"></img>
+          </div>
+        </div>
+      </div>
+      <div id="container">
+        <img id="image" src={logo4}></img>
+        <div id="text-container">
+          <h1>55</h1>
+          <p>Haught or Naught</p>
+          <p>High-minded or absent-minded?You decide.</p>
+          <div id='profile'>
+            <p>Submitted by: </p>
+            <img id="profile-img" src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"></img>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
