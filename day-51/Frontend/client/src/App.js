88e8c1f6d4d63d47b20f91d132633effa6bf4e01
JamiyanD <115760624+JamiyanD@users.noreply.github.com>
2023-01-26 22:05:07 +0800

    express backend gerte zuer shalgasin

--- day-51/Frontend/client/src/App.js
+++ day-51/Frontend/client/src/App.js
@@ -1,61 +1,65 @@
-
-import {useEffect, useState} from 'react'
-import './App.css';
-import InputForm from './Component/InputForm';
-const GET_DATA_URL = 'http://localhost:8080/data'
-const DELETE_DATA_URL = 'http://localhost:8080/data'
+import { useEffect, useState } from "react";
+import "./App.css";
+import InputForm from "./Component/InputForm";
+const GET_DATA_URL = "http://localhost:8080/data";
+const DELETE_DATA_URL = "http://localhost:8080/data";
 function App() {
-  const [data, setData] = useState([])
-  const [isLoading,setisLoading] =useState(false)
+  const [data, setData] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  console.log(data);
 
-  async function fetchData (){
-   const FETCHED_DATA = await fetch(GET_DATA_URL)
-   const FETCHED_JSON = await FETCHED_DATA.json()
-    console.log(FETCHED_JSON)
-      setData(FETCHED_JSON)
-  
+  async function fetchData() {
+    const FETCHED_DATA = await fetch(GET_DATA_URL);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    setData(FETCHED_JSON);
   }
-  async function deleteData(data){
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function deleteData(data) {
     const options = {
       method: "DELETE",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-    }
-    const FETCHED_DATA = await fetch(DELETE_DATA_URL, options)
-    const FETCHED_JSON = await FETCHED_DATA.json()
-    setData(FETCHED_JSON)
+      body: JSON.stringify(data),
+    };
+    const FETCHED_DATA = await fetch(DELETE_DATA_URL, options);
+    console.log(FETCHED_DATA);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    console.log(FETCHED_JSON);
+    setData(FETCHED_JSON);
   }
 
- useEffect(() => {
-  fetchData()
- },[])
-
- function handleDelete(id){
-  console.log(id)
-  const data = {
-    id: id
+  function handleDelete(id) {
+    const data = {
+      id: id,
+    };
+    deleteData(data);
   }
-  deleteData(data)
- }
   return (
     <div className="App">
-     <h1>Day-51 - React/Express FullStack APP - without Database</h1>
-     <InputForm isLoading={isLoading} 
-     setisLoading={setisLoading} 
-     setData={setData}/>
-     {
-      isLoading ? '...Loading' : (data && data.map((d,index) => {
-        return (
-          <div key={index}>
-          <p >{d.name} -- {d.major}</p>
-          <button onClick={() => handleDelete(d.id)}>Delete</button>
-          </div>
-        )
-       }))
-     }
-  
+      <h1>Day-51 - React/Express FullStack APP - without Database</h1>
+      <InputForm
+        isLoading={isLoading}
+        setisLoading={setisLoading}
+        setData={setData}
+      />
+      {isLoading
+        ? "...Loading"
+        : data &&
+          data.map((d, index) => {
+            return (
+              <div key={index}>
+                <p>
+                  {d.name} -- {d.major}
+                </p>
+                <button onClick={() => handleDelete(d.id)}>Delete</button>
+              </div>
+            );
+          })}
     </div>
   );
 }
