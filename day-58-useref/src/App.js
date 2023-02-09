06e44cd105b913bea368c8a028988f1a23adb21d
JamiyanD <duurenjargaljamyan@gmail.com>
2023-02-09 10:52:23 +0800

    video

--- day-58-useref/src/App.js
+++ day-58-useref/src/App.js
@@ -3,12 +3,16 @@ import List from "./Components/List";
 import { useContext } from "react";
 import { ImageContext } from "./Context/ImageContext";
 import Stopwatch from "./Components/Stopwatch";
+import { InputFocus } from "./Components/InputFocus";
+import VideoPlayer from "./Components/VideoPlayer";
 function App() {
   const [isLarge, setIsLarge, imageSize] = useContext(ImageContext);
 
   return (
     <div className="App">
       <Stopwatch />
+      <InputFocus />
+      <VideoPlayer />
       {/* <input
         type="checkbox"
         value={isLarge}
